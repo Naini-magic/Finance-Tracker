35248b0b11bb2e09f22d852e992ce0a92f13b42a
Sunaini Raghuwanshi <144336156+Naini-magic@users.noreply.github.com>
2025-09-10 20:10:11 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     // Use environment variable for MongoDB connection
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI  || "mongodb+srv://raghuwanshisunaini_db_user:emSfcN2A3R0DDubM@cluster0.qek8eex.mongodb.net/" , {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
